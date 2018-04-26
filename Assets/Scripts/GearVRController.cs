48a6e1b1ffc322802f5b6e0506632aca0f9f4841
A. Loonstra <arnaud@sphaero.org>
2018-04-26 15:41:13 +0200

    initial assets commit

---
+++ Assets/Scripts/GearVRController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Controlles : MonoBehaviour {
+
+	public GameObject world;
+
+	void Start () {
+		OVRTouchpad.Create();
+		OVRTouchpad.TouchHandler += HandleTouchHandler;
+
+	}
+	void HandleTouchHandler(object sender, System.EventArgs e)
+	{
+		OVRTouchpad.TouchArgs touchArgs = (OVRTouchpad.TouchArgs)e;
+		if (touchArgs.TouchType == OVRTouchpad.TouchEvent.SingleTap)
+		{
+			world.SetActive( !world.activeInHierarchy );
+		}
+	}
+
+	void Update () {
+
+	}
+}
---
+++ Assets/Scripts/OSCRecvr.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OSCRecvr : MonoBehaviour {
+
+	public OSC osc;
+	public UnityEngine.Video.VideoPlayer vplayer;
+	public UnityEngine.UI.Text bboard;
+	public UnityEngine.TextMesh board3d;
+
+	// Use this for initialization
+	void Start () {
+		vplayer.loopPointReached += onLoopPointReached;
+		Debug.Log(Network.player.ipAddress);
+		bboard.text = Network.player.ipAddress;
+		board3d.text = Network.player.ipAddress;
+		osc.SetAddressHandler( "/play" , OnReceivePlay );
+		osc.SetAddressHandler("/stop", OnReceiveStop);
+		osc.SetAddressHandler("/pause", OnReceivePause);
+		osc.SetAddressHandler("/restart", OnReceiveRestart);
+		osc.SetAddressHandler ("/ipon", OnReceiveIpon);
+		osc.SetAddressHandler ("/ipoff", OnReceiveIpoff);
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	void onLoopPointReached( UnityEngine.Video.VideoPlayer vp) {
+		Debug.Log ("VideoEnd");
+		vp.Stop ();
+	}
+
+	void OnReceivePlay(OscMessage message){
+		Debug.Log ("Play");
+		vplayer.Play ();
+	}
+
+	void OnReceiveStop(OscMessage message) {
+		Debug.Log ("Stop");
+		vplayer.Stop ();
+	}
+
+	void OnReceivePause(OscMessage message) {
+		Debug.Log ("Pause");
+		vplayer.Pause ();
+	}
+
+	void OnReceiveRestart(OscMessage message) {
+		Debug.Log ("Restart");
+		vplayer.Stop ();
+		vplayer.Play ();
+	}
+
+	void OnReceiveIpon(OscMessage message) {
+		Debug.Log (Network.player.ipAddress);
+		bboard.text = Network.player.ipAddress;
+		board3d.text = Network.player.ipAddress;
+		bboard.gameObject.SetActive (true);
+		board3d.gameObject.SetActive (true);
+	}
+
+	void OnReceiveIpoff(OscMessage message) {
+		Debug.Log (Network.player.ipAddress);
+		bboard.text = Network.player.ipAddress;
+		board3d.text = Network.player.ipAddress;
+		bboard.gameObject.SetActive (false);
+		board3d.gameObject.SetActive (false);
+	}
+
+}
